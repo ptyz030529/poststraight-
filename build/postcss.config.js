d2716ce89dc736c8fb97a54c2bae55c3ba2d34b5
JimbaChen <429000366@qq.com>
2017-08-17 17:12:50 +0800

    update

--- build/postcss.config.js
+++ build/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = ({ file, options, env }) => ({
   plugins: {
     "postcss-import": {},
+    "precss": {},
     "postcss-cssnext": {
       browsers: ['last 2 versions', 'IE > 10']
     },
